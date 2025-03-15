6a12c2866776fa02cba17c34b34e039a1ed6e3fa
hezeAI-Art <enocstioffan@gmail.com>
2025-03-15 23:52:21 +0800

    Add files via upload

---
+++ js/jsk.js
@@ -0,0 +1,23 @@
+const apiKey = "YOUR_TMDB_API_KEY";
+const baseURL = "https://api.themoviedb.org/3";
+
+async function getMovies() {
+    const response = await fetch(`${baseURL}/movie/popular?api_key=${apiKey}`);
+    const data = await response.json();
+    
+    const moviesList = document.getElementById("movies-list");
+    moviesList.innerHTML = "";
+
+    data.results.forEach(movie => {
+        const movieItem = document.createElement("div");
+        movieItem.classList.add("movie-item");
+        movieItem.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <p>${movie.title}</p>
+            <a href="https://moviebox.ph/embed/${movie.id}" target="_blank">Watch Now</a>
+        `;
+        moviesList.appendChild(movieItem);
+    });
+}
+
+getMovies();
